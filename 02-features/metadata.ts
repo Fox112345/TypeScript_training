3d077bed9eeca95b755cbc631fd7e306a544e997
Artem <Fox112345@yandex.ru>
2020-07-03 14:28:41 +0300

    metadata

---
+++ 02-features/metadata.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+
+class Plane {
+  color: string = "red";
+
+  @markFunction("ergerge")
+  fly(): void {
+    console.log("vrrrrr");
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata("secret", secretInfo, target, key);
+  }
+}
+
+const secret = Reflect.getMetadata("secret", Plane.prototype, "fly");
+
+console.log(secret);
